b62c0e4c731ceb98928f4a3e47b642092a6fff52
Shahabr99 <sean.rohani9@gmail.com>
2023-06-14 18:09:05 -0700

    Shopping list js done

--- static/app.js
+++ static/app.js
@@ -1,3 +1,29 @@
 "use strict";
-const shoppingItem = document.querySelector(".shopping-list");
-const checkbox = document.querySelector("#check");
+const shoppingItem = document.querySelectorAll(".shopping-list");
+const checkboxs = document.querySelectorAll(".check");
+const shoppingItemArr = Array.from(shoppingItem);
+const checkboxsArr = Array.from(checkboxs);
+
+let click = 0;
+
+// checkbox.addEventListener("click", function (e) {
+//   if (e.target === checkbox) {
+//     e.target.parentElement.classList.add("clicked");
+//     click += 1;
+//   }
+//   if (click % 2 === 0) {
+//     e.target.parentElement.classList.remove("clicked");
+//   }
+// });
+
+for (let checkbox of checkboxs) {
+  checkbox.addEventListener("click", function (e) {
+    if (e.target.className === "check") {
+      e.target.parentElement.classList.add("clicked");
+      click += 1;
+    }
+    if (click % 2 === 0) {
+      e.target.parentElement.classList.remove("clicked");
+    }
+  });
+}
